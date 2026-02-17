2cf74b6873ee7b42b14d722b1cb82d47a59a6e61
kubsikora <kubsikora@gmail.com>
2026-02-17 23:49:39 +0100

    fix

--- config/database.php
+++ config/database.php
@@ -45,22 +45,34 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => null,
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'strict' => false,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                (PHP_VERSION_ID >= 80500 ? \Pdo\Mysql::ATTR_SSL_CA : \PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+    'host' => '127.0.0.1',
+    'database' => 'vh14167_test',
+    'username' => 'vh14167_test',
+    'password' => 'obrazeknascianie123@',
+    'charset' => 'utf8',            // Zmień z utf8mb4 na utf8
+    'collation' => 'utf8_general_ci',
+    'strict' => false,
+    'options' => [
+        \PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+        \PDO::ATTR_EMULATE_PREPARES => true, // To może naprawić problem z \u0000
+    ],
+            // 'driver' => 'mysql',
+            // 'url' => null,
+            // 'host' => env('DB_HOST', '127.0.0.1'),
+            // 'port' => env('DB_PORT', '3306'),
+            // 'database' => env('DB_DATABASE', 'laravel'),
+            // 'username' => env('DB_USERNAME', 'root'),
+            // 'password' => env('DB_PASSWORD', ''),
+            // 'unix_socket' => env('DB_SOCKET', ''),
+            // 'charset' => 'utf8',
+            // 'collation' => 'utf8_unicode_ci',
+            // 'prefix' => '',
+            // 'prefix_indexes' => true,
+            // 'strict' => false,
+            // 'engine' => null,
+            // 'options' => extension_loaded('pdo_mysql') ? array_filter([
+            //     (PHP_VERSION_ID >= 80500 ? \Pdo\Mysql::ATTR_SSL_CA : \PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
+            // ]) : [],
         ],
 
         'mariadb' => [
